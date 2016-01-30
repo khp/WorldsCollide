a0cb60b2b99d17032584c070ca7362cb8bb49915
khp <kanghee.p@gmail.com>
2016-01-30 11:21:55 -0800

    Start Player script﻿

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	[SerializedField] private int playerNum;
+	public int favour;
+	public int potential;
+	public string choice;
+	public boolean gameOn;
+	private Dictionary<string,string> choiceRes;
 	// Use this for initialization
 	void Start () {
-	
+		favour = 0;
+		potential = 0;
+		choice = null;
+		choiceRes = new Dictionary<string,string> ();
+		if (playerNum = 1) {
+			choiceRes.Add ("1", "mouse");
+			choiceRes.Add ("2", "cat");
+			choiceRes.Add ("3", "elephant");
+		} else {
+			choiceRes.Add ("8", "mouse");
+			choiceRes.Add ("9", "cat");
+			choiceRes.Add ("0", "elephant");
+		}
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (choice != null)
+			return;
+		foreach (string s in choiceRes.Keys) {
+			if (Input.GetKey (s)) {
+				choice = choiceRes.get (s);
+			}
+		}
+	}
+
+	void ResetChoice () {
+		choice = null;
 	}
 }
