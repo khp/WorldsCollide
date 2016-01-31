d1350a6ee9e4e7c39e9c8fa7be863a8b46eddecb
Daniel Weinshenker <weinshenkerdaniel@gmail.com>
2016-01-30 19:41:55 -0800

    initial script for status bar

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatusBar : MonoBehaviour {
+
+	[SerializeField] private Image gauge;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void UpdateValue (int value) {
+		gauge.GetComponent<RectTransform>.localScale = new Vector2(((200 * value) / 20), 100);
+	}
+}
