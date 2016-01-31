8715a9d16066a93baf7b57a0ec39a3b7696adac2
khp <kanghee.p@gmail.com>
2016-01-31 13:49:39 -0800

    update animals

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -6,6 +6,15 @@ public class Animal : MonoBehaviour {
 	public Sprite cat;
 	public Sprite mouse;
 	public Sprite elephant;
+	public string animalType;
+
+	public AudioSource catAlive;
+	public AudioSource catDead;
+	public AudioSource mouseAlive;
+	public AudioSource mouseDead;
+	public AudioSource elephantAlive;
+	public AudioSource elephantDead;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +23,7 @@ public class Animal : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		SetSprite ();
 	}
 
 	public void ThrowRight() {
@@ -30,5 +39,36 @@ public class Animal : MonoBehaviour {
 	public void Hide() {
 		Vector2 oldVector = GetComponent<Transform> ().position;
 		GetComponent<Transform> ().position = new Vector3 (oldVector.x, oldVector.y, 1);
+	}
+
+	public void RegularSound() {
+		if (animalType == "cat")
+			catAlive.Play ();
+		else if (animalType == "mouse")
+			mouseAlive.Play ();
+		else if (animalType == "elephant")
+			elephantAlive.Play ();
+	}
+
+	public void DeathSound() {
+		if (animalType == "cat")
+			catDead.Play ();
+		else if (animalType == "mouse")
+			mouseDead.Play ();
+		else if (animalType == "elephant")
+			elephantDead.Play ();
+	}
+
+	public void SetSprite () {
+		Sprite currentSprite;
+		if (animalType == "cat")
+			currentSprite = cat;
+		else if (animalType == "mouse")
+			currentSprite = mouse;
+		else if (animalType == "elephant")
+			currentSprite = elephant;
+		else
+			currentSprite = null;
+		GetComponent<SpriteRenderer> () = currentSprite;
 	}
 }
